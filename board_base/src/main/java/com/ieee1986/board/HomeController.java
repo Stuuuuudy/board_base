090ce35859ac55e86bf98d7ff9b1a1878c9c2e3c
jeonjehun <jeonjehun@EssencePC>
2017-06-10 00:13:36 +0900

    3. 프로젝트 생성 - Spring Framework 1차 버전업 - pom.xml에 jackson library dependency 추가 - 동작 테스트성 코드 구성 - 추후 스프링 버전업과 auto 설정을 수동으로 설정하는것을 진행해야 한다.

--- board_base/src/main/java/com/ieee1986/board/HomeController.java
+++ board_base/src/main/java/com/ieee1986/board/HomeController.java
@@ -1,39 +1,37 @@
 package com.ieee1986.board;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ieee1986.board.model.User;
 
 /**
  * Handles requests for the application home page.
  */
-@Controller
+@RestController
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public ResponseEntity<User> home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+
+		User user = new User();
+		user.setId("aaa");
+		user.setName("bbb");
+		return new ResponseEntity<User>(user, HttpStatus.OK);
 	}
-	
+
 }
