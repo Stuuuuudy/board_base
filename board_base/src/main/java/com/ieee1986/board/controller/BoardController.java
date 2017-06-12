8f934ee210563c51ee59e16e8d4d0808c7f0a57d
jeonjehun <jeonjehun@EssencePC>
2017-06-12 23:30:05 +0900

    3. 프로젝트 생성 - 샘플 컨트롤러 생성 - 로그 설정 동작 확인

---
+++ board_base/src/main/java/com/ieee1986/board/controller/BoardController.java
@@ -0,0 +1,8 @@
+package com.ieee1986.board.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+
+}
--- board_base/src/main/java/com/ieee1986/board/model/User.java
+++ board_base/src/main/java/com/ieee1986/board/model/User.java
@@ -6,6 +6,11 @@ public class User {
 
 	private String name;
 
+	public User(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
 	public String getId() {
 		return id;
 	}
